1b1814f580a72008e1b0dad6310b50bce4454b76
kodunaiff <kodunaiff@gmail.com>
2024-06-12 16:41:22 +0800

    setup rollbar

--- restaurateur/urls.py
+++ restaurateur/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', lambda request: redirect('restaurateur:ProductsView')),
 
     path('products/', views.view_products, name="ProductsView"),
+    path('products2/', views.index, name="IndexView"),
 
     path('restaurants/', views.view_restaurants, name="RestaurantView"),
 
